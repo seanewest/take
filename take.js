b3b77a7988e4878472d8f78c562b30fb7e5da464
Aleksandrs Ulme <aulme@thoughtworks.com>
2014-04-06 00:04:36 +0100

    refactored the code

--- take.js
+++ take.js
@@ -1,17 +1,27 @@
 var $ = require('jquery');
 
-window.take = function(packageName) {
+function makeUrl(packageName, version) {
+  var mainUrl = 'http://wzrd.in/bundle/';
+  var versionUrlSegment = version ? "@" + version : "";
+  return mainUrl + packageName + versionUrlSegment;
+}
+
+function checkForErrors(packageName, version) {
   if (!packageName) {throw new Error('please provide a package name');};
 
-  var url = 'http://wzrd.in/bundle/' + packageName;
-  var code = $.ajax({
+}
+
+function syncGet(url) {
+  return $.ajax({
     type: "GET",
     url: url,
     async: false
-  }).responseText;
+  });
+}
 
-  var result = eval(code)(packageName);
-  console.log(packageName + " loaded");
-
-  return result;
+window.take = function(packageName, version) {
+  checkForErrors(packageName, version);
+  var url = makeUrl(packageName, version);
+  var response = syncGet(url);
+  return eval(response.responseText)(packageName);
 }
--- take_bundle.js
+++ take_bundle.js
@@ -9114,20 +9114,30 @@ return jQuery;
 },{}],2:[function(require,module,exports){
 var $ = require('jquery');
 
-window.take = function(packageName) {
+function makeUrl(packageName, version) {
+  var mainUrl = 'http://wzrd.in/bundle/';
+  var versionUrlSegment = version ? "@" + version : "";
+  return mainUrl + packageName + versionUrlSegment;
+}
+
+function checkForErrors(packageName, version) {
   if (!packageName) {throw new Error('please provide a package name');};
 
-  var url = 'http://wzrd.in/bundle/' + packageName;
-  var code = $.ajax({
+}
+
+function syncGet(url) {
+  return $.ajax({
     type: "GET",
     url: url,
     async: false
-  }).responseText;
+  });
+}
 
-  var result = eval(code)(packageName);
-  console.log(packageName + " loaded");
-
-  return result;
+window.take = function(packageName, version) {
+  checkForErrors(packageName, version);
+  var url = makeUrl(packageName, version);
+  var response = syncGet(url);
+  return eval(response.responseText)(packageName);
 }
 
 },{"jquery":1}]},{},[2])
